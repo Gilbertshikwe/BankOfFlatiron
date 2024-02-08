f009c61be6d899d5bdad6e475a189adb6d6e028e
Gilbert <shikweg@gmail.com>
2024-02-08 12:42:43 +0300

    added Navbar elements

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddTransactionForm from '../components/AddTransactionForm';
+
+const AddTransaction = () => {
+  return (
+    <div>
+      <h2>Add Transaction</h2>
+      <AddTransactionForm />
+    </div>
+  );
+};
+
+export default AddTransaction;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Flatiron Bank</h2>
+      <p>
+        This is the main page of Flatiron Bank. Explore other sections using the
+        navigation bar.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
